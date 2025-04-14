0bdad5d2447a735af142ac8f43d24f8ec0213924
salimi <salimiiz888@gmail.com>
2025-04-14 19:00:47 +0330

    The digikalamag page is complete

---
+++ js/myjs.js
@@ -0,0 +1,333 @@
+// document ready
+$(document).ready(function () {
+    // هنگام بارگذاری صفحه، موقعیت اسکرول را بازیابی کنیم  
+    if (sessionStorage.getItem('scrollPosition')) {
+        $(window).scrollTop(sessionStorage.getItem('scrollPosition'));
+    }
+
+    // هنگام اسکرول، موقعیت اسکرول را ذخیره کنیم  
+    $(window).on('scroll', function () {
+        sessionStorage.setItem('scrollPosition', $(window).scrollTop());
+    });
+
+    // Berger menu  
+    const $button = $('.navbar-toggler');
+    const $navOverlay = $('.nav-overlay');
+    const $navbarCollapse = $('.navbar-collapse');
+
+    $button.on('click', function () {
+        $navOverlay.toggleClass('active');
+    });
+
+    $navOverlay.on('click', function (event) {
+        if (event.target === this) {
+            $navOverlay.removeClass('active');
+            $navbarCollapse.removeClass('show');
+        }
+    });
+
+
+    // close button
+    $('.mobail-fixed-bottom .btn-close').on('click', function () {
+        $('.mobail-fixed-bottom').css("display", "none");
+    });
+
+
+    // header swiper
+    var swiperheader = new Swiper(".headerSwiper", {
+        slidesPerView: 3,
+        slidesPerGroup: 1,
+        centeredSlides: false,
+        loop: false,
+        spaceBetween: 15,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        on: {                    // رویدادها  
+            init: function () {
+                updateNavigationButtons(this);
+            },
+            slideChange: function () {
+                updateNavigationButtons(this);
+            }
+        }
+
+    });
+
+
+    // carousel in m1
+
+
+    var swiper1 = new Swiper(".mobailSwiper", {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        centeredSlides: false,
+        loop: true,
+        spaceBetween: 0,
+        speed: 1000,              // تنظیم سرعت جابجایی (به میلی‌ثانیه)  
+
+        autoplay: {              // تنظیمات autoplay  
+            delay: 12000,         // زمان تاخیر برای autoplay  
+            disableOnInteraction: false, // غیر فعال نشود بعد از تعامل کاربر  
+        },
+        pagination: {            // تنظیمات pagination  
+            el: '.swiper-pagination', // انتخاب عنصر برای pagination  
+            clickable: true,          // کلیک‌پذیر بودن دکمه‌های pagination  
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        on: {                    // رویدادها  
+            init: function () {
+                updateNavigationButtons(this);
+            },
+            slideChange: function () {
+                updateNavigationButtons(this);
+            }
+        }
+
+    });
+
+
+
+    // media watch in m3
+
+    $('.media-list-item').click(function (e) {
+        e.preventDefault(); // جلوگیری از رفتن به لینک  
+        var videoId = $(this).data('video'); //ویدیو id  دریافت 
+
+        // مخفی کردن همه ویدیوها  
+        $('.media-item').hide();
+
+        // نمایش ویدیوی انتخاب شده  
+        $('#' + videoId).show();
+
+        // حذف کلاس "active" از همه عناصر ویدیو  
+        $('.media-list-item').removeClass('active');
+
+        // اضافه کردن کلاس "active" به عنصر انتخاب شده  
+        $(this).addClass('active');
+
+    });
+
+    $('.big-play-btn').on('click', function () {
+        var video = $(this).closest('.m3-video-items').find('.video-item')[0];
+        video.play(); // ویدیو را پخش کنید  
+        $(this).hide(); // دکمه را پنهان کنید  
+        $(this).closest('.m3-video-items').find('.img-video').hide(); // تصویر را پنهان کنید  
+    });
+
+    // وقتی کاربر اسکرول کند، دکمه جستجو نشان داده شود  
+    $(window).scroll(function () {
+        var saidscrollTop = $(this).scrollTop(); // مقدار اسکرول از بالای صفحه  
+        // console.log($(this).scrollTop())
+        if (saidscrollTop < 1730) {
+            // $('.saidbar-icon').fadeOut();
+            $('.saidbar-icon').addClass("position-absolute1");
+
+
+        }
+        if (saidscrollTop > 1730) {
+            $('.saidbar-icon').fadeIn();
+            $('.saidbar-icon').css("position" , "statick");
+            $('.saidbar-icon').removeClass("position-absolute1");
+
+
+
+        } 
+        if (saidscrollTop > 1900) {
+            $('.saidbar-icon').removeClass("position-absolute1");
+            $('.saidbar-icon').addClass("fixed");
+            console.log($(this).scrollTop())
+
+        } else {  
+            $('#sidebar').removeClass('fixed'); // حذف کلاس fixed 
+            $('.saidbar-icon').addClass("position-absolute1");
+
+            $('#sidebar').css('top', '2000px'); // نشانی دایو در موقعیت اولیه  
+        }  
+
+    });
+
+
+    // وقتی کاربر اسکرول کند، دکمه نشان داده شود  
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $('#scrollToTop').fadeIn();
+        } else {
+            $('#scrollToTop').fadeOut();
+        }
+    });
+
+    // وقتی دکمه کلیک شد، به بالا اسکرول کنیم  
+    $('#scrollToTop').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 400);
+        return false;
+    });
+
+    $('.saidbar-menu li').hover(function () {
+        $('.overlay').css("display", "block");
+    }, function () {
+        $('.overlay').css("display", "none");
+
+    }
+    );
+
+
+    // video swiper
+    var swiperheader = new Swiper(".videoSwiper", {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        centeredSlides: false,
+        loop: false,
+        spaceBetween: 15,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            350: {
+                slidesPerView: 1,
+                slidesPerGroup: 2,
+            },
+            430: {
+                slidesPerView: 1.5,
+                slidesPerGroup: 2,
+            },
+            567: {  // از 768 پیکسل به بالا  
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+            }
+        },
+        on: {                    // رویدادها  
+            init: function () {
+                updateNavigationButtons(this);
+            },
+            slideChange: function () {
+                updateNavigationButtons(this);
+            }
+        }
+
+    });
+
+
+    var currentVideo = null;
+    $('.mobail-big-play-btn').on('click', function () {
+        var video = $(this).closest('.m4-mobail-video-items').find('.mobail-video-item')[0];
+        if (currentVideo && currentVideo !== video) {
+            currentVideo.pause();
+            currentVideo.currentTime = 0;
+        }
+        video.play();
+        currentVideo = video;
+        $(this).hide();
+        $(this).closest('.m4-mobail-video-items').find('.mobail-img-video').hide();
+    });
+
+
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        slidesPerGroup: 1,
+        centeredSlides: false,
+        loop: false,
+        spaceBetween: 10,
+        speed: 2000,              // تنظیم سرعت جابجایی (به میلی‌ثانیه)  
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        touchEventsTarget: 'container',
+        breakpoints: {
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 10,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            }
+        },
+
+        on: {
+            init: function () {
+                updateNavigationButtons(this);
+            },
+            slideChange: function () {
+                updateNavigationButtons(this);
+            }
+        }
+    });
+    // $('.swiper-button-prev').hide(); // مخفی کردن دکمه  
+
+
+    function updateNavigationButtons(swiper) {
+
+        // مخفی کردن دکمه قبلی در اولین آیتم  
+        if (swiper.isBeginning) {
+            $('.swiper-button-prev').hide();
+        } else {
+            $('.swiper-button-prev').show();
+        }
+
+        // مخفی کردن دکمه بعدی در آخرین آیتم  
+        if (swiper.isEnd) {
+            $('.swiper-button-next').hide();
+        } else {
+            $('.swiper-button-next').show();
+        }
+    }
+
+    updateNavigationButtons(swiper);
+    // console.log(swiper.isBeginning)
+
+    // $('.swiper-button-prev').attr('aria-disabled', 'true');  
+
+
+    // m6 swiper
+    var swiperm6 = new Swiper(".m6Swiper", {
+        slidesPerView: 1.5,
+        slidesPerGroup: 1,
+        centeredSlides: false,
+        loop: false,
+        spaceBetween: 15,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            350: {
+                slidesPerView: 1,
+                slidesPerGroup: 2,
+            },
+            430: {
+                slidesPerView: 1.5,
+                slidesPerGroup: 2,
+            },
+            768: {  // از 768 پیکسل به بالا  
+                slidesPerView: 2.5,
+                slidesPerGroup: 2,
+            }
+        },
+
+        on: {                    // رویدادها  
+            init: function () {
+                updateNavigationButtons(this);
+            },
+            slideChange: function () {
+                updateNavigationButtons(this);
+            }
+        }
+
+    });
+
+
+
+});
---
+++ js/news.js
@@ -0,0 +1,403 @@
+const news = [
+    { 
+        "id" : 3 , 
+        "href" : "" , 
+        "category" : "دکوراتیو" , 
+        "title" : "راهنمای دکوراسیون ترندی خانه با رنگ سال ۲۰۲۵ پنتون" ,
+        "user" : "نسرین طاهری پور" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/3.jpg" , "/images/img4/u3.jpg"] ,
+        "coment" : "" 
+    } , 
+    {
+        "id" : 2 , 
+        "href" : "" , 
+        "category" : "موبایل" , 
+        "title" : "اپل به اولین شرکت ۴ تریلیون دلاری جهان تبدیل می‌شود" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/2.jpg " , "/images/img4/u1.jpg"] ,
+        "coment" : ""
+    } , 
+    {
+        "id" : 1 , 
+        "href" : "" , 
+        "category" : "هوش مصنوعی و روباتیک" , 
+        "title" : "OpenAI برنامه‌های خود را برای تبدیل به شرکت انتفاعی مشخص کرد" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/1.jpg " ,"/images/img4/u1.jpg" ],
+        "coment" : ""
+  
+    } , 
+    {
+        "id" : 4 , 
+        "href" : "" , 
+        "category" : "بازی و سرگرمی" , 
+        "title" : "آموزش نصب بازی روی پلی‌استیشن ۵" ,
+        "user" : "وحید گلباد" , 
+        "discription" : "   " , 
+        "duration" : "6دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/4.jpg " , "/images/img4/u4.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 5 , 
+        "href" : "" , 
+        "category" : "تلویزیون" , 
+        "title" : "وصل کردن گوشی به تلویزیون اسنوا چگونه انجام می‌شود؟" ,
+        "user" : "حمید پورحسینی" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/5.jpg " , "/images/img4/u5.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 6 , 
+        "href" : "" , 
+        "category" : "هوش مصنوعی و روباتیک" , 
+        "title" : "OpenAI برنامه‌های خود را برای تبدیل به شرکت انتفاعی مشخص کرد" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/1.jpg " ,"/images/img4/u1.jpg" ],
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 7 , 
+        "href" : "" , 
+        "category" : "موبایل" , 
+        "title" : "اپل به اولین شرکت ۴ تریلیون دلاری جهان تبدیل می‌شود" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/2.jpg " , "/images/img4/u1.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 8 , 
+        "href" : "" , 
+        "category" : "دکوراتیو" , 
+        "title" : "راهنمای دکوراسیون ترندی خانه با رنگ سال ۲۰۲۵ پنتون" ,
+        "user" : "نسرین طاهری پور" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/3.jpg " , "/images/img4/u3.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 9 , 
+        "href" : "" , 
+        "category" : "بازی و سرگرمی" , 
+        "title" : "آموزش نصب بازی روی پلی‌استیشن ۵" ,
+        "user" : "وحید گلباد" , 
+        "discription" : "   " , 
+        "duration" : "6دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/4.jpg " , "/images/img4/u4.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 10 , 
+        "href" : "" , 
+        "category" : "تلویزیون" , 
+        "title" : "وصل کردن گوشی به تلویزیون اسنوا چگونه انجام می‌شود؟" ,
+        "user" : "حمید پورحسینی" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/5.jpg " , "/images/img4/u5.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+     
+    {
+        "id" : 11 , 
+        "href" : "" , 
+        "category" : "هوش مصنوعی و روباتیک" , 
+        "title" : "OpenAI برنامه‌های خود را برای تبدیل به شرکت انتفاعی مشخص کرد" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/1.jpg " ,"/images/img4/u1.jpg" ],
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 12 , 
+        "href" : "" , 
+        "category" : "موبایل" , 
+        "title" : "اپل به اولین شرکت ۴ تریلیون دلاری جهان تبدیل می‌شود" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/2.jpg " , "/images/img4/u1.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 13 , 
+        "href" : "" , 
+        "category" : "دکوراتیو" , 
+        "title" : "راهنمای دکوراسیون ترندی خانه با رنگ سال ۲۰۲۵ پنتون" ,
+        "user" : "نسرین طاهری پور" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/3.jpg " , "/images/img4/u3.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 14 , 
+        "href" : "" , 
+        "category" : "بازی و سرگرمی" , 
+        "title" : "آموزش نصب بازی روی پلی‌استیشن ۵" ,
+        "user" : "وحید گلباد" , 
+        "discription" : "   " , 
+        "duration" : "6دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/4.jpg " , "/images/img4/u4.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 15 , 
+        "href" : "" , 
+        "category" : "تلویزیون" , 
+        "title" : "وصل کردن گوشی به تلویزیون اسنوا چگونه انجام می‌شود؟" ,
+        "user" : "حمید پورحسینی" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/5.jpg " , "/images/img4/u5.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 16 , 
+        "href" : "" , 
+        "category" : "هوش مصنوعی و روباتیک" , 
+        "title" : "OpenAI برنامه‌های خود را برای تبدیل به شرکت انتفاعی مشخص کرد" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/1.jpg " ,"/images/img4/u1.jpg" ],
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 17 , 
+        "href" : "" , 
+        "category" : "موبایل" , 
+        "title" : "اپل به اولین شرکت ۴ تریلیون دلاری جهان تبدیل می‌شود" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/2.jpg " , "/images/img4/u1.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 18 , 
+        "href" : "" , 
+        "category" : "دکوراتیو" , 
+        "title" : "راهنمای دکوراسیون ترندی خانه با رنگ سال ۲۰۲۵ پنتون" ,
+        "user" : "نسرین طاهری پور" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/3.jpg " , "/images/img4/u3.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 19 , 
+        "href" : "" , 
+        "category" : "بازی و سرگرمی" , 
+        "title" : "آموزش نصب بازی روی پلی‌استیشن ۵" ,
+        "user" : "وحید گلباد" , 
+        "discription" : "   " , 
+        "duration" : "6دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/4.jpg " , "/images/img4/u4.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 20 , 
+        "href" : "" , 
+        "category" : "تلویزیون" , 
+        "title" : "وصل کردن گوشی به تلویزیون اسنوا چگونه انجام می‌شود؟" ,
+        "user" : "حمید پورحسینی" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/5.jpg " , "/images/img4/u5.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+     
+    {
+        "id" : 21 , 
+        "href" : "" , 
+        "category" : "هوش مصنوعی و روباتیک" , 
+        "title" : "OpenAI برنامه‌های خود را برای تبدیل به شرکت انتفاعی مشخص کرد" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/1.jpg " ,"/images/img4/u1.jpg" ],
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 22 , 
+        "href" : "" , 
+        "category" : "موبایل" , 
+        "title" : "اپل به اولین شرکت ۴ تریلیون دلاری جهان تبدیل می‌شود" ,
+        "user" : "یوسف اسفندیاری" , 
+        "discription" : "   " , 
+        "duration" : "2دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/2.jpg " , "/images/img4/u1.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 23 , 
+        "href" : "" , 
+        "category" : "دکوراتیو" , 
+        "title" : "راهنمای دکوراسیون ترندی خانه با رنگ سال ۲۰۲۵ پنتون" ,
+        "user" : "نسرین طاهری پور" , 
+        "discription" : "   " , 
+        "duration" : "12دقیقه مطالعه " ,
+        "time" : "1 ساعت قبل" ,
+        "img" : ["/images/img4/3.jpg " , "/images/img4/u3.jpg"] ,
+        "coment" : ""
+        
+  
+    } , 
+    {
+        "id" : 24 , 
+        "href" : "" , 
+        "category" : "بازی و سرگرمی" , 
+        "title" : "آموزش نصب بازی روی پلی‌استیشن ۵" ,
+        "user" : "وحید گلباد" , 
+        "discription" : "   " , 
+        "duration" : "6دقیقه مطالعه " ,
+        "time" : "11 ساعت قبل" ,
+        "img" : ["/images/img4/4.jpg " , "/images/img4/u4.jpg"] ,
+        "coment" : ""
+        
+  
+    } 
+]
+
+
+
+
+
+
+
+
+// display all of news   
+displayNew(news);
+
+
+
+//display of news
+function   displayNew(newItems){
+  
+  let displayNew = newItems.map(function(item){
+        return  ` <div class="m4-item card col-lg-4 col-md-4 col-sm-6 col-6 p-0 me-4  textnew1" data-id="${item.id}">
+        <div class="m4-item-img position-relative">
+        <span class="m4-img-mask"></span>
+        <img src="${item.img[0]}" class="news-img1 w-100 h-100 card-img-top" alt="محصول 1" height="180" width="284">
+        <a href="#" class="m4-category fs-13">${item.category}</a>
+        <span class="m4-duration f-vazir-fanum  me-2">${item.duration}</span>
+        <div class="icon-clock d-inline-block">
+            <svg width="15px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                    <path d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 
+                    7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                </g>
+            </svg>
+        </div>
+        </div> 
+        
+
+        <div class="card-body d-flex flex-column justify-content-between pb-0 pt-4">  
+            <h6 class="card-title m4-item-title h-25 fw-normal f-vazir-fanum"><a href="#" class=""> ${item.title}</a></h6> 
+            <div class="m4-card-des py-4 text-center align-items-center f-vazir-fanum fs-11 fw-medium d-flex">
+            <div class="user-slide w-60 rounded-4  text-center ">
+            <img src="${item.img[1]}" class=" img-user rounded-4 " alt="">
+            <span class="icon-user"> <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="10" r="3" stroke="#b0b0b0" stroke-linecap="round"></circle> <circle cx="12" cy="12" r="9" stroke="#b0b0b0"></circle> <path d="M18 18.7059C17.6461 17.6427 16.8662 16.7033 15.7814 16.0332C14.6966 15.3632 13.3674 15 12 15C10.6326 15 9.30341 15.3632 8.21858 16.0332C7.13375 16.7033 6.35391 17.6427 6 18.7059" stroke="#b0b0b0" stroke-linecap="round"></path> </g></svg></span> 
+            <span class=""> ${item.user}</span>
+            </div> 
+            <div class="list-item-time h-50 ps-3 f-vazir-fanum">
+                    <div class="icon-clock d-inline-block">
+                        <svg width="15px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#afb6bc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </g>
+                        </svg>
+                    </div>
+                    <span class="f-vazir-fanum">${item.time}</span>
+                    <a href="" class=""> </a>
+             </div> 
+            </div>
+        </div>  
+        </div>`
+  });
+  
+  displayNew = displayNew.join('');
+  document.querySelector(".news-items").innerHTML = displayNew ;
+  
+}
+
+
